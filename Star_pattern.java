f5d978528dc14bd3cc359b5421d6784567f0e43d
Ayush Mishra <137910810+Ayush31120@users.noreply.github.com>
2023-06-29 00:16:08 +0530

    Add files via upload

---
+++ Star_pattern.java
@@ -0,0 +1,12 @@
+public class Star_pattern {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i = 1; i<=6;i++){
+            System.out.println();
+            for(int j = 1; j<=i;j++){
+                System.out.print("*");
+            }
+        }
+    }
+    
+}
---
+++ Star_pattern_descending.java
@@ -0,0 +1,12 @@
+public class Star_pattern_descending {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 5; i>=1;i--){
+            System.out.println();
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+        }
+    }
+    
+}
